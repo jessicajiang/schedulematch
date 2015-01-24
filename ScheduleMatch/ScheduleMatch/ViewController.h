80ee25f01bb05913f31b2a53c5e947a05e9b430c
Jessica Jiang <jessicajiang5@gmail.com>
2015-01-24 14:46:49 -0800

    first

---
+++ ScheduleMatch/ScheduleMatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScheduleMatch
+//
+//  Created by Jessica Jiang on 1/24/15.
+//  Copyright (c) 2015 Jessica Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScheduleMatch/ScheduleMatch/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ScheduleMatch
+//
+//  Created by Jessica Jiang on 1/24/15.
+//  Copyright (c) 2015 Jessica Jiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
