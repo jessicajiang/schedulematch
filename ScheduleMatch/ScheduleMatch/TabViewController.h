b30c5e96609be435178e57f1437db4ff02d937e2
koroket <bmasakazu@gmail.com>
2015-01-24 15:34:02 -0800

    tab skeleton

--- ScheduleMatch/ScheduleMatch/TabViewController.h
+++ ScheduleMatch/ScheduleMatch/TabViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  TabViewController.h
 //  ScheduleMatch
 //
-//  Created by Jessica Jiang on 1/24/15.
+//  Created by sloot on 1/24/15.
 //  Copyright (c) 2015 Jessica Jiang. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TabViewController : UITabBarController
 
 @end
-
---
+++ ScheduleMatch/ScheduleMatch/TabViewController.m
@@ -0,0 +1,38 @@
+//
+//  TabViewController.m
+//  ScheduleMatch
+//
+//  Created by sloot on 1/24/15.
+//  Copyright (c) 2015 Jessica Jiang. All rights reserved.
+//
+
+#import "TabViewController.h"
+
+@interface TabViewController ()
+
+@end
+
+@implementation TabViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setSelectedIndex:1];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
